5a99e95e5190abdc47fc8b9d9cd71b0f3a3eb078
waanee kim (호이호이) <ksw654@gmail.com>
2019-03-24 01:58:44 +0900

    nux

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,7 @@
       <h1 class="title">
         temamoaVue
       </h1>
-      <h2 class="subtitle">
-        My phenomenal Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      이제 개발환경이 구축 되었다!
     </div>
   </section>
 </template>
